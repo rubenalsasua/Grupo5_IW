189c6bd52b5e9506837b5e1fd6e43858ed8b04ef
asiernava <nava.asier@opendeusto.es>
2024-05-02 15:52:19 +0200

    añadir datos bbdd y nueva funcion create

--- appDeustotil_Tech/urls.py
+++ appDeustotil_Tech/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
-from appDeustotil_Tech.views import EmpleadoDetailView, EmpleadoListView
-from appDeustotil_Tech.views import ProyectoDetailView
+from appDeustotil_Tech.views import EmpleadoDetailView, EmpleadoListView, EmpleadoCreateView
+from appDeustotil_Tech.views import ProyectoDetailView, ProyectoListView
 from appDeustotil_Tech.views import TareaListView
 
 urlpatterns = [
     # ej: /miApp/
     path('', views.index, name='index'),
-    path('/', views.index_proyectos, name='Proyectos'),
+    path('/proyectos', ProyectoListView.as_view(), name='Proyectos'),
     path('/empleado/<int:pk>', EmpleadoDetailView.as_view(), name="empleado-show"),
     path('/empleado', EmpleadoListView.as_view() , name = "empleado-list"),
+    path('/empleado/create', EmpleadoCreateView.as_view(), name = "empleado-list"),
     path('/proyecto/<int:pk>', ProyectoDetailView.as_view(), name = "proyecto-show"),
     path('/tarea', TareaListView.as_view(), name = "tarea-list")
 ]
--- appDeustotil_Tech/views.py
+++ appDeustotil_Tech/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, View, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, View, DeleteView, UpdateView, CreateView
+from appDeustotil_Tech.forms import EmpleadoForm
 from appDeustotil_Tech.models import Proyecto
 from appDeustotil_Tech.models import Empleado
 from appDeustotil_Tech.models import Tarea
@@ -9,14 +10,16 @@ from appDeustotil_Tech.models import Tarea
 
 # Create your views here.
 
-#PROYECTOS VIEWS
 def index(request):
     return HttpResponse("Esta es nuestra aplicación de Django!")
 
+#PROYECTOS VIEWS
 
-def index_proyectos(request, proyecto_id):
-    proyectos = get_object_or_404(Proyecto, id=proyecto_id)
-    return render(request, 'appDeustotil_Tech/proyecto_list.html', {'proyectos': proyectos})
+class ProyectoListView(ListView):
+    model = Proyecto
+    template_name = 'appDeustotil_Tech/proyecto_list.html'
+    context_object_name = 'proyectos'
+
 
 class ProyectoDetailView(DetailView):
     model = Proyecto
@@ -35,6 +38,20 @@ class EmpleadoListView(ListView):
     model = Empleado
     template_name = 'appDeustotil_Tech/empleado_list.html'
     context_object_name = 'empleado'
+
+class EmpleadoCreateView(View):
+    def get(self, request):
+        formulario = EmpleadoForm()
+        context = {'formulario': formulario}
+        return render(request, 'appDeustotil_Tech/empleado_create.html', context)
+
+    def post(self, request):
+        formulario = EmpleadoForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('index')
+        return render(request, 'appDeustotil_Tech/empleado_create.html', {'formulario': formulario})
+
 
 
 #TAREA VIEWS
